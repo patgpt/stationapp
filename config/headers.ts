3e28f386ceb960370d7dae589b3e99117a405233
patgpt <channel.42@icloud.com>
2023-08-15 11:02:45 -0700

    fix: :bug: this is bad .

--- config/headers.ts
+++ config/headers.ts
@@ -28,13 +28,14 @@ const securityHeaders = [
       value: 'no-referrer',
     },
   ];
-  
+  export default securityHeaders;
   module.exports = async () => {
     return [
       {
         // Apply these headers to all routes in your application.
         source: '/:path*',
-        headers: securityHeaders,
+        headers:  securityHeaders,
       },
     ];
   };
+  
